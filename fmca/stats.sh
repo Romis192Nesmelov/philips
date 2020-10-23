9c5dd0461c49de2ee51a8d47d945717ea313b826
Viktor Makarov <viktor.makarov@corp.mail.ru>
2020-10-23 20:24:00 +0300

    Fix: Change logic

--- fmca/stats.sh
+++ fmca/stats.sh
@@ -6,15 +6,14 @@ STATSDIR=/usr/share/nginx/html/fmca/stats
 if [ ! -d $STATSDIR ]
 then
   mkdir $STATSDIR
-  chmod 777 $STATSDIR
-  chgrp -R www-data $STATSDIR
-  chown -R www-data $STATSDIR
+  # chmod 777 $STATSDIR
+  # chgrp -R www-data $STATSDIR
+  # chown -R www-data $STATSDIR
 fi
 
 rm -f $STATSDIR/*
 
-mysql --database=philips -u root < stats1.sql | sed 's/\t/;/g' > $FMCADIR/stats/stat_action.csv
-mysql --database=philips -u root -e "SOURCE $FMCADIR/stats.sql"
+mysql --database=philips -u root < stats.sql | sed 's/\t/;/g' > $FMCADIR/stats/stat_action.csv
 python3 "$FMCADIR/send_stats.py"
 
 # crontab -e

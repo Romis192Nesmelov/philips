e93d464dd243391fc4e34dc55b215a1d4c2903bc
Viktor Makarov <viktor.makarov@corp.mail.ru>
2020-10-23 20:06:37 +0300

    Test

---
+++ fmca/stats2.sql
@@ -0,0 +1,22 @@
+SELECT 'Date',
+       'Registrations', 
+       'Promocodes', 
+       'Discount10', 
+       'Discount20', 
+       'Discount30', 
+       'Discount40', 
+       'Discount50', 
+       'Discount60', 
+       'Orders';
+
+
+
+
+
+
+
+
+
+
+
+

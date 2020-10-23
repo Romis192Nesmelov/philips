6af9ef7a39e798f5bd13b05ca5f551289dad3f46
Viktor Makarov <viktor.makarov@corp.mail.ru>
2020-10-23 20:28:14 +0300

    Test new

--- fmca/backup_mysql.sh
+++ fmca/backup_mysql.sh
@@ -7,13 +7,13 @@ date=`date "+%Y-%m-%d-%H%M%S"`
 if [ ! -d $BACKUPDIR ]
 then
   mkdir $BACKUPDIR
-  chmod 777 $BACKUPDIR
-  chgrp -R www-data $BACKUPDIR
-  chown -R www-data $BACKUPDIR
+  # chmod 777 $BACKUPDIR
+  # chgrp -R www-data $BACKUPDIR
+  # chown -R www-data $BACKUPDIR
 fi
 
 find $BACKUPDIR/* -mmin +$((60*24)) -exec rm {} \;
-mysqldump --single-transaction -u philips -p******** --all-databases | gzip > $BACKUPDIR/$date-backup.sql.gz
+mysqldump --single-transaction --all-databases | gzip > $BACKUPDIR/$date-backup.sql.gz
 
 # crontab -e
 # 15,45 * * * * /usr/share/nginx/html/fmca/backup_mysql.sh

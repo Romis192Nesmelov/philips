5c450fe8661a48ea5716b1f30a5f223c61b71d8a
Viktor Makarov <viktor.makarov@corp.mail.ru>
2020-10-30 14:43:23 +0300

    Fix text for reg

--- resources/views/auth/confirm_register.blade.php
+++ resources/views/auth/confirm_register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.auth')
 
 @section('content')
-<p class="info-message">Проверьте свою почту, Вам было выслано письмо с подтверждением регистрации!</p>
+<p class="info-message">Проверьте свою почту, Вам было выслано письмо с подтверждением регистрации. Проверьте также спам, письмо могло попасть туда.</p>
 <div class="icon-close"></div>
 <script>$('#pop-up').show();</script>
 @endsection

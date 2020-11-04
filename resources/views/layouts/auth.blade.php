f9ec3e9f5abc13aac5994cfe4bd359a67afb0c1e
Viktor Makarov <viktor.makarov@corp.mail.ru>
2020-11-04 18:10:01 +0300

    Fix text

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -120,7 +120,7 @@
 
         <img class="gifts" src="/images/gifts.jpg" />
         <div class="terms">
-            @include('_term_block', ['num' => 1, 'text' => '<b>Заправься</b><br>от 10 литров<br>Shell V-Power'])
+            @include('_term_block', ['num' => 1, 'text' => '<b>Заправься</b><br>топливом Shell<br>от 10 литров<sup>1</sup>'])
             @include('_term_block', ['num' => 2, 'text' => '<b>Купи</b><br>любой кофе<br>deli by Shell'])
             @include('_term_block', ['num' => 3, 'text' => 'И ты <nobr>в игре!</nobr>', 'addClass' => 'final'])
         </div>
